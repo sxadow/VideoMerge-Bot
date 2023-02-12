3286ec16c19e311d962b0e1b28c14c3808f76b86
sxadow <107715054+sxadow@users.noreply.github.com>
2023-02-12 13:29:24 +0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,20 +4,20 @@ import os
 
 
 class Config(object):
-    API_ID = os.environ.get("API_ID")
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = os.environ.get("5166878")
+    API_HASH = os.environ.get("fdafb41f9a67f40e34a6c67f47730a92")
+    BOT_TOKEN = os.environ.get("6010108695:AAHc0YwD1gb0ORLbkiTa9w7iNEY58CRi4XM")
     SESSION_NAME = os.environ.get("SESSION_NAME", "Video-Merge-Bot")
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
+    UPDATES_CHANNEL = os.environ.get("-1001405282390")
+    LOG_CHANNEL = os.environ.get("-1001405282390")
     DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
     TIME_GAP = int(os.environ.get("TIME_GAP", 5))
     MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 5))
     STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME")
     STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
-    MONGODB_URI = os.environ.get("MONGODB_URI")
+    MONGODB_URI = os.environ.get("mongodb+srv://1111:1111@cluster0.s9jjabf.mongodb.net/?retryWrites=true&w=majority")
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1445283714))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", 762308466))
 
     START_TEXT = """
 Hi Unkil, I am Video Merge Bot!
